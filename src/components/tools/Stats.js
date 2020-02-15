4eeef27d8c1ba0f7232776fa50514255337b8730
xZeroW <vic_marts@hotmail.com>
2020-02-15 17:11:39 -0300

    update

--- src/components/tools/Stats.js
+++ src/components/tools/Stats.js
@@ -30,59 +30,57 @@ function Stats() {
 
   return (
     <div className="container">
-      <div className="row">
-        <div className="col s12 m12">
-          <table className="table table-borderless">
-            <tbody>
-            <tr>
-              <td>Strength:</td>
-              <td>
-                <input type="number" name="str" 
-                onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
-                value={str} onChange={(e) => setStr(e.target.value)} /></td>
-              <td>{parseInt(str) + 15}</td>
-            </tr>
-            <tr>
-              <td>Dexterity:</td>
-              <td>
-                <input type="number" name="dex" 
-                onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
-                value={dex} onChange={(e) => setDex(e.target.value)} /></td>
-              <td>{parseInt(dex) + 15}</td>
-            </tr>
-            <tr>
-              <td>Vitality:</td>
-              <td>
-                <input type="number" name="vit" 
-                onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
-                value={vit} onChange={(e) => setVit(e.target.value)} /></td>
-              <td>{parseInt(vit) + 15}</td>
-            </tr>
-            <tr>
-              <td>Spirituality:</td>
-              <td>
-                <input type="number" name="spr" 
-                onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
-                value={spr} onChange={(e) => setSpr(e.target.value)} /></td>
-              <td>{parseInt(spr) + 15}</td>
-            </tr>
-            <tr>
-              <td>Character Level:</td>
-              <td>
-                <input type="number" name="cLevel" 
-                onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
-                value={cLevel} onChange={handleCLevel} /></td>
-            </tr>
-            <tr>
-              <td>Points Available:</td>
-              <td>{cpAv}</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
+      <div className="stats">
+      <table className="table table-borderless table-sm table-responsive">
+        <tbody>
+        <tr>
+          <th scope="row">Strength:</th>
+          <td>
+            <input type="number" name="str" 
+            onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
+            value={str} onChange={(e) => setStr(e.target.value)} /></td>
+          <td>{parseInt(str) + 15}</td>
+        </tr>
+        <tr>
+        <th scope="row">Dexterity:</th>
+          <td>
+            <input type="number" name="dex" 
+            onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
+            value={dex} onChange={(e) => setDex(e.target.value)} /></td>
+          <td>{parseInt(dex) + 15}</td>
+        </tr>
+        <tr>
+        <th scope="row">Vitality:</th>
+          <td>
+            <input type="number" name="vit" 
+            onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
+            value={vit} onChange={(e) => setVit(e.target.value)} /></td>
+          <td>{parseInt(vit) + 15}</td>
+        </tr>
+        <tr>
+        <th scope="row">Spirituality:</th>
+          <td>
+            <input type="number" name="spr" 
+            onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
+            value={spr} onChange={(e) => setSpr(e.target.value)} /></td>
+          <td>{parseInt(spr) + 15}</td>
+        </tr>
+        <tr>
+        <th scope="row">Character Level:</th>
+          <td>
+            <input type="number" name="cLevel" 
+            onKeyDown={ e => ( e.keyCode === 69 || e.keyCode === 190 || e.keyCode === 188 ) && e.preventDefault() } 
+            value={cLevel} onChange={handleCLevel} /></td>
+        </tr>
+        <tr>
+          <th scope="row">Points Available:</th>
+          <td>{cpAv}</td>
+        </tr>
+        </tbody>
+      </table>
       </div>
     </div>
-  );
+    );
   }
 
 export default Stats;
--- src/components/tools/Tree.js
+++ src/components/tools/Tree.js
@@ -2,90 +2,103 @@ import React, { useState } from 'react';
 
 function Tree() {
   const [pbar, setPbar] = useState(0);
-  function handlePbar(){
-    if(pbar < 55){
-      setPbar(pbar + 1);
+  const [pAvailable, setPAvailable] = useState(177);
+  const [skills, setSkills] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+  function handlePbar() {
+    if (pbar < 55){
+      setPbar(pbar+1);
+      setPAvailable(pAvailable-1);
+      document.getElementById('crupbar').style.height = pbar * 1.85 + '%';
     }
   }
+
+  const handleSkills = (index) => {
+    // setSkills(skills[index] + 1)
+    console.log(skills[index])
+  }
+
   return (
-    <div class="container">
-      <p>Work In Progress</p>
+    <div className="container">
+      <p>Skill Tree Builder - <span class="badge badge-warning">WIP</span></p>
+      <p>Points Available: {pAvailable}</p>
       <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-          <a class="nav-item nav-link active" id="crusader-tab" data-toggle="tab" href="#crusader" role="tab" aria-controls="crusader" aria-selected="true">Crusader</a>
-          <a class="nav-item nav-link" id="hunter-tab" data-toggle="tab" href="#hunter" role="tab" aria-controls="hunter" aria-selected="false">Hunter</a>
-          <a class="nav-item nav-link" id="exorcist-tab" data-toggle="tab" href="#exorcist" role="tab" aria-controls="exorcist" aria-selected="false">Exorcist</a>
-          <a class="nav-item nav-link" id="magus-tab" data-toggle="tab" href="#magus" role="tab" aria-controls="magus" aria-selected="false">Magus</a>
+        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+          <a className="nav-item nav-link active" id="crusader-tab" data-toggle="tab" href="#crusader" role="tab" aria-controls="crusader" aria-selected="true">Crusader</a>
+          <a className="nav-item nav-link" id="hunter-tab" data-toggle="tab" href="#hunter" role="tab" aria-controls="hunter" aria-selected="false">Hunter</a>
+          <a className="nav-item nav-link" id="exorcist-tab" data-toggle="tab" href="#exorcist" role="tab" aria-controls="exorcist" aria-selected="false">Exorcist</a>
+          <a className="nav-item nav-link" id="magus-tab" data-toggle="tab" href="#magus" role="tab" aria-controls="magus" aria-selected="false">Magus</a>
         </div>
       </nav>
-      <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade show active" id="crusader" role="tabpanel" aria-labelledby="crusader-tab">
+      <div className="tab-content" id="nav-tabContent">
+        <div className="tab-pane fade show active" id="crusader" role="tabpanel" aria-labelledby="crusader-tab">
           <table>
+            <tbody>
             <tr>
-              <td rowspan="5">
+              <td rowSpan="5">
                 <div className="progress progress-bar-vertical" onClick={handlePbar}>
-                  <div className="progress-bar" id="pbar"  role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="55">{pbar}
+                  <div className="progress-bar" id="crupbar"  role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="55">{pbar}
                   </div>
                 </div>
               </td>
-              <td id="Aries"><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td id="WildBlow"><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td id="Aries"><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[17]}</div></td>
+              <td id="WildBlow"><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[18]}</div></td>
               <td></td>
-              <td id="FountainOfYouth"><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td id="IronMaiden"><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td id="FountainOfYouth"><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[19]}</div></td>
+              <td id="IronMaiden"><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[20]}</div></td>
               <td></td>
-              <td id="Entrenche"><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td id="BloodRush"><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td id="Entrenche"><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[21]}</div></td>
+              <td id="BloodRush"><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[22]}</div></td>
             </tr>
             <tr>
               <td></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[14]}</div></td>
               <td></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[15]}</div></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[16]}</div></td>
             </tr>
             <tr>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[10]}</div></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[11]}</div></td>
               <td></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[12]}</div></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[13]}</div></td>
               <td></td>
             </tr>
             <tr>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[5]}</div></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[6]}</div></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[7]}</div></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[8]}</div></td>
               <td></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[9]}</div></td>
             </tr>
             <tr>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td onClick={e => setSkills({...skills, skills: 5})}><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[0]}</div></td>
               <td></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
-              <td><img src="https://via.placeholder.com/42" alt="skill"></img></td>
+              <td onClick={handleSkills}><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[1]}</div></td>
+              <td onClick={handleSkills}><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[2]}</div></td>
+              <td onClick={handleSkills}><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[3]}</div></td>
+              <td onClick={handleSkills}><img src="https://via.placeholder.com/42" alt="skill"></img><div>{skills[4]}</div></td>
               <td></td>
               <td></td>
             </tr>
+            </tbody>
           </table>
         </div>
-        <div class="tab-pane fade" id="hunter" role="tabpanel" aria-labelledby="hunter-tab">
+        <div className="tab-pane fade" id="hunter" role="tabpanel" aria-labelledby="hunter-tab">
           hunter
         </div>
-        <div class="tab-pane fade" id="exorcist" role="tabpanel" aria-labelledby="exorcist-tab">
+        <div className="tab-pane fade" id="exorcist" role="tabpanel" aria-labelledby="exorcist-tab">
           exorcist
         </div>
-        <div class="tab-pane fade" id="magus" role="tabpanel" aria-labelledby="magus-tab">
+        <div className="tab-pane fade" id="magus" role="tabpanel" aria-labelledby="magus-tab">
           magus
         </div>
       </div>
